1735894d4d4f6486f44ed610b3d848ceef2dc795
Jose Luis Flores Rivera <jlflores1999@gmail.com>
2021-01-31 17:39:05 -0600

    Cambio de valor de monedas

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-export var score: = 100
+export var score: = -1
 
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
