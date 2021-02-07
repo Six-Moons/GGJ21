3866bf01bda42b8cfc989acc35d86a8920f84507
Alberto <albertogarv1402@gmail.com>
2021-02-07 14:20:06 -0600

    Changed camera limits, changed levels to fit camera limits and reimported assets

--- src/Levels/Level.gd
+++ src/Levels/Level.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var tile_size = 32
 export(String, FILE) var next_scene_path: = ""
+export(int) var num_tiles_bottom = 24
+export(int) var num_tiles_right = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Player/Camera2D.limit_right = num_tiles_right * tile_size
+	$Player/Camera2D.limit_bottom = num_tiles_bottom * tile_size
 	PlayerData.change_scene(next_scene_path)
 	pass # Replace with function body.
 
